b26e258fe394fae584ced9d38cb80a9344393889
dubs3c <dubs3c@users.noreply.github.com>
2020-12-15 15:59:37 +0100

    save

--- assignment_4/compile.sh
+++ assignment_4/compile.sh
@@ -4,7 +4,7 @@ echo '[+] Assembling with Nasm ... '
 nasm -f elf32 -o build/$1.o $1.nasm
 
 echo '[+] Linking ...'
-ld -o build/$1 build/$1.o
+ld -z execstack -N -o build/$1 build/$1.o
 
 echo '[+] Done!'
 
